20db6793f7f6c3986d8396b0e7e02e54896a6134
Joselyn Chavez <joselynchavezf@gmail.com>
2024-01-09 20:05:45 -0600

    update

--- week_2/Assignment2.R
+++ week_2/Assignment2.R
@@ -1,10 +1,10 @@
-### Introduction to Epidemiology Data Analysis with R. 
+### Introduction to Epidemiology Data Analysis with R.
 
 ### Introduction to coding with R. Part I.
 
-# Assignment week 2: 
+# Assignment week 2:
 
-## 1) Which of the following are good variable names?
+## 1) Which of the following options are good variable names?
 
 a) Patient$age
 b) cause_of_disease
@@ -20,27 +20,18 @@ b <- "hello"
 c <- TRUE
 
 ### 2) What is the class of vector a?
-class(a)
 
 ### 3) What is the class of vector c?
-class(c)
 
 ## Store the result of the following code in a new vector
-d <- c(a,b,c)
 
 ### 4) What is the resulting class of the new vector?
-class(d)
 
 ## Let's create a vector that contains the weight of three patients:
 patients_weight <- c(198, 165, 150)
 
 ### 5) How do you get the number of patients in the vector?
-length(patients_weight)
 
 ### 6) How would you get the weight of patient 2?
-patients_weight[2]
 
 ### 7) How would you get the weight of patients 2 and 3?
-patients_weight[2:3]
-patients_weight[c(2,3)]
-patients_weight[seq(from = 2, to = 3)]
---
+++ week_2/code_week_2.R
@@ -0,0 +1,142 @@
+my_vector_1 <- 10
+my_vector_1
+
+my_vector_2 <- "a"
+my_vector_2
+
+my_vector_3 <- c(1,5,10)
+my_vector_3
+
+my_vector_4 <- c("a", "," ,"c", "f")
+my_vector_4
+
+paste("a","b", sep = ";")
+
+vector_1 <- 300
+vector_2 <- 1:500
+vector_2
+vector_3 <- c("a","b","c","d","e")
+vector_3
+vector_4 <- c("Joselyn", "32", "Irapuato")
+vector_4
+vector_5 <- c("I'm Joselyn", "I'm a postdoc")
+vector_5
+
+1:5
+
+seq(from = 1, to = 500, by = 10)
+
+vector_numeric <- 10
+vector_integer <- 5L
+
+vector_numeric
+vector_integer
+
+vector_character <- "my name"
+vector_character
+
+class(vector_numeric)
+class(vector_integer)
+class(vector_character)
+class(vector_logical)
+
+vector_logical <- FALSE
+vector_logical
+
+vector_factor <- factor("a")
+vector_factor
+
+
+vector_4 <- c("Joselyn", 32, "Irapuato")
+
+c("Joselyn", 32, "Irapuato")
+
+vector_numeric
+vector_character
+
+class(vector_4)
+
+vector_6 <- c("a", TRUE, FALSE)
+class(vector_6)
+vector_6
+
+vector_7 <- c(0, 10, TRUE)
+vector_7
+class(vector_7)
+
+vector_8 <- c(0, 1, TRUE)
+vector_8
+class(vector_8)
+
+vector_9 <- c(10, 20, 50)
+class(vector_9)
+
+vector_9 <- as.character(vector_9)
+vector_9
+
+as.numeric(vector_character)
+
+vector_4
+as.numeric(vector_4)
+
+vector_8
+as.logical(vector_8)
+
+vector_10 <- c(FALSE, TRUE, TRUE)
+class(vector_10)
+
+as.numeric(vector_10)
+as.character(vector_10)
+
+patient_age <- c(27, 30, 5, NA)
+patient_name <- c("John", "Sam", "Nicole", "Will")
+patient_weight <- c(100, 160, 170, 180)
+
+as.numeric("a")
+
+log10(-10)
+
+vector_11 <- c(2, 87, -20)
+log(vector_11)
+
+patient_age <- c(27, 30, 5, NA)
+patient_name <- c("John", "Sam", "Nicole", "Will")
+patient_weight <- c(100, 160, 170, 180)
+
+
+patient_name[3]
+patient_weight[4]
+
+patient_name[1:2]
+patient_name[2:4]
+
+patient_age[c(1,3)]
+
+patient_age <- c(27, 30, 5, NA)
+patient_name <- c("John", "Sam", "Nicole", "Will")
+patient_weight <- c(100, 160, 170, 180)
+
+patient_weight[c(1,2,4)]
+
+vector_12 <- seq(from = 50, to = 100, by = 5)
+vector_12
+
+vector_12[1:7]
+vector_12[3:11]
+
+length(vector_12)
+
+LETTERS
+
+vector_13 <- c("a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k")
+
+vector_13 <- letters[1:11]
+
+vector_13
+
+# c,d,j
+vector_13[c(3,4,10)]
+
+
+
+
