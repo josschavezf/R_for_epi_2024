4083df2a5b78cf3e7a9b9e8fa0a2534602ab0c23
josschavezf <joselynchavezf@gmail.com>
2024-03-05 13:41:47 -0500

    add assignment

---
+++ week_9/Assignment9.R
@@ -0,0 +1,33 @@
+### Introduction to Epidemiology Data Analysis with R. 
+
+### Data visualization
+
+# Assignment week 9
+
+### Import Sinai_covid.csv and covid2.csv and join the 2 datasets using full_join()
+
+### Exercise 1 (We did that in class, try to improve the plot): 
+
+# - Create a violin plot to visualize IQ scores by facility
+# - Add jittered points
+# - Change the theme
+# - Create subplots by sex
+# - What else can you add to improve this plot?
+# - Store this plot in an object called “violin”
+
+### Exercise 2: 
+
+# - Create a density plot to visualize the diastolic bp 
+# - Color by smoking status
+# - Use a different color scale than the default
+# - Change the theme
+# - Create subplots by sex
+# - What else can you add to improve this plot? 
+# - Store this plot in an object called “dens”
+
+### Exercise 3: 
+
+# - Combine plots “violin” and “dense”, one under the other
+# - Save it as pdf
+# - upload the pdf to Blackboard
+
