c5ad63d3ddacb5cec86e5a699096f925ea1e2542
josschavezf <joselynchavezf@gmail.com>
2024-03-06 11:55:05 -0500

    add assignment

---
+++ week_10/Assignment10.R
@@ -0,0 +1,11 @@
+### Introduction to Epidemiology Data Analysis with R. 
+
+### Modeling
+
+# Assignment week 10
+
+### Import Sinai_covid.csv and covid2.csv and join the 2 datasets using full_join()
+
+### What links between variables would be interesting to explore in the sinai_covid dataset?
+# 1 - Choose 2 or more variables you think might be linked
+# 2 - Plot, fit, and transform to explore the relationship between them
